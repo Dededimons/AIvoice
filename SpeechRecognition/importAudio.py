7f9ae869d00f64586d61b445d4573f2a6ed66774
erikturkovic <60065884+Dededimons@users.noreply.github.com>
2024-10-28 21:57:02 +0100

    Testing speech recognition

---
+++ SpeechRecognition/importAudio.py
@@ -0,0 +1,21 @@
+import torch
+import librosa
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+
+file_path = r"TestFiles\test.wav" 
+audio, sample_rate = librosa.load(file_path, sr=16000)
+
+input_values = torch.tensor(audio).unsqueeze(0) 
+
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+
+inputs = processor(audio, sampling_rate=16000, return_tensors="pt", padding=True)
+
+with torch.no_grad():
+    logits = model(inputs.input_values).logits
+
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = processor.decode(predicted_ids[0])
+
+print("Transcribed Text in Croatian:", transcription)
